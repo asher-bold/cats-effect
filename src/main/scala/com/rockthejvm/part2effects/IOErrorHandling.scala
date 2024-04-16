78e535e4d5d58c246ab7d0caa1ac8ba314df4d74
Asher Weinberg <AsherW@BoldIntel.com>
2024-04-16 13:43:32 +0300

    remove solution

--- src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
@@ -29,29 +29,20 @@ object IOErrorHandling {
    */
   // 1 - construct potentially failed IOs from standard data types (Option, Try, Either)
   def option2IO[A](option: Option[A])(ifEmpty: Throwable): IO[A] =
-    option match {
-      case Some(value) => IO.pure(value)
-      case None => IO.raiseError(ifEmpty)
-    }
+
 
   def try2IO[A](aTry: Try[A]): IO[A] =
-    aTry match {
-      case Success(value) => IO.pure(value)
-      case Failure(ex) => IO.raiseError(ex)
-    }
+
 
   def either2IO[A](anEither: Either[Throwable, A]): IO[A] =
-    anEither match {
-      case Left(ex) => IO.raiseError(ex)
-      case Right(value) => IO.pure(value)
-    }
+
 
   // 2 - handleError, handleErrorWith
   def handleIOError[A](io: IO[A])(handler: Throwable => A): IO[A] =
-    io.redeem(handler, identity)
+
 
   def handleIOErrorWith[A](io: IO[A])(handler: Throwable => IO[A]): IO[A] =
-    io.redeemWith(handler, IO.pure)
+
 
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global
